aebd55caa4ea1979266933307cd8e8c269f06d5a
NgKh10 <nguyenngockhuee10@gmail.com>
2026-05-07 17:01:37 +0700

    ok

--- obj/Debug/net8.0/knjewelry.AssemblyInfo.cs
+++ obj/Debug/net8.0/knjewelry.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("knjewelry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587bca6446aa661230a39b73fa0dd94308a1013b")]
 [assembly: System.Reflection.AssemblyProductAttribute("knjewelry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("knjewelry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
